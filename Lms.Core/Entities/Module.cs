5cedbe5d9616b0edbffc91fcf15b17989684fbc7
hleivav <hleiva@hotmail.com>
2022-08-30 10:20:52 +0200

    Add project files.

---
+++ Lms.Core/Entities/Module.cs
@@ -0,0 +1,10 @@
+﻿namespace Lms.Core.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public int CourseId { get; set; }
+    }
+}
